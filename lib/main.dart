53d824c32594abad7ce6ca366f9e1e9ede8c6309
ez23re <ez23re@gmail.com>
2023-12-01 18:38:39 +0900

    테스트용

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-/// 방가방가
-// 지히ㅣ안녕
---
+++ lib/이세진_연습.dart
@@ -0,0 +1,7 @@
+String name = "이순신";
+int age = 20;
+
+void main() {
+  print('$name은 $age살 입니다.');
+  print('$name은 ${name.length}글자 입니다.');
+}
